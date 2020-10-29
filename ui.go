7e195f0e75b11a24a9877e5d06c60acf190c5463
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-10-29 14:09:12 +0530

    docs: add godocs

--- ui.go
+++ ui.go
@@ -1,3 +1,4 @@
+// Package ui provides few easy to use api methods to write clean outputs with styling support along with emojis
 package ui
 
 import (
@@ -7,7 +8,8 @@ import (
 	"github.com/kyokomi/emoji"
 )
 
-// ContextPrint is used to display message with emoji context
+// ContextPrint writes the output in the format of "[emoji] : message".
+// a complete list of supported emojis available here : http://www.unicode.org/emoji/charts/emoji-list.html
 func ContextPrint(emojiName string, message string) {
 	var ending string
 	if len(message) == 0 {
@@ -19,13 +21,15 @@ func ContextPrint(emojiName string, message string) {
 	emoji.Printf("[ :%s:] %s %s", emojiName, message, ending)
 }
 
-// PrintLink with dimmed formatting
+// PrintLink is used to style printing links to the console, dimmed, with a hyperlink icon
 func PrintLink(context string, link string) {
 	styleCyan := color.New(color.FgCyan).SprintFunc()
 	emoji.Printf("[ :link:] %s %s \n", context, styleCyan(link))
 }
 
-// CheckError and display error message
+// CheckError shortens your code. Simply pass the error variable into the function,
+// the error message your want to display if error was not nil, and finally the
+// third parameter describes if you want to halt the execution of the program if err.
 func CheckError(e error, message string, quit bool) {
 	var emoji string
 
